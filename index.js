cfd6ca13355d00afa6131bd7f27e03f524ef102b
widesteal321 <71954340+widesteal321@users.noreply.github.com>
2021-07-15 20:51:49 +1200

    Cleaned up code
    Just removed some things that were not needed and potentially slowed down the process

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
-const fs = require('fs');
+const fs = require('fs'); // Used for reading and writing to files
+const { csvParse } = require('csv-string').parse; // Used to parse to an array
 const dir = fs.opendirSync('./messages');
-const csvParse = require('csv-string').parse;
 
-let dirent; 
-let dirent2;
+let dirent = null; 
 
 while ((dirent = dir.readSync()) !== null) {
+  if (dirent.name === "index.json") break; // index.json is TYPICALLY the final file in this folder, it is also not a folder.
+  fs.readFile(`./messages/${dirent.name}/messages.csv`, 'utf8', function (err,data) { // Read the csv file
+    let data2;
+    if (err) console.log(err); // You know the drill, if it errors, tell the user
+    data.replace('"', '“'); // Used to prevent errors in the JSON in relation to quotation characters
+    data2 = csvParse(data); // Parse to an array
+    data2 = data2.map(array => array.filter((_, index) => (index + 1) % 3 === 0)); // Removes dates, and message ids, remove if you wish to keep
 
-  if (dirent.name === "index.json") break; 
-  let dir2 = fs.opendirSync(`./messages/${dirent.name}`);
-
-  while ((dirent2 = dir2.readSync()) !== null) {
-    if (dirent2.name === "messages.csv") {
-      fs.readFile(`./messages/${dirent.name}/messages.csv`, 'utf8', function (err,data) {
-
-        if (err) console.log(err);
-        data.replace('"', '“'); // used to prevent errors in the JSON 
-        let data2 = csvParse(data);
-        data2 = data2.map(array => array.filter((_, index) => (index + 1) % 3 === 0)); //removes dates, and message ids, remove if you wish to keep
-
-        fs.writeFile('information.json', JSON.stringify(data2), function (err) {
-          if (err) console.log(err);
-        });
-      });
-    }
-  }
-  
-  dir2.closeSync();
+    fs.writeFile('information.json', JSON.stringify(data2), function (err) { // Write the csv file contents to the JSON file
+       if (err) console.log(err); // Ditto...
+    });
+  });
 }
-
 dir.closeSync();
-console.log("Completed, check information.json.")
+
+console.log("Completed, check information.json.") // Tell the user the script is complete

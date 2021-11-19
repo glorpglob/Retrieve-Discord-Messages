98e5a7d253c544123448930743c12cd6863fb76b
widesteal321 <71954340+widesteal321@users.noreply.github.com>
2021-11-19 18:36:18 +1300

    fixed csvParse function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('fs'); // Used for reading and writing to files
-const { csvParse } = require('csv-string').parse; // Used to parse to an array
+const csvParse = require('csv-string').parse; // Used to parse to an array
 const dir = fs.opendirSync('./messages');
 
 let dirent = null; 
@@ -11,7 +11,7 @@ while ((dirent = dir.readSync()) !== null) {
     if (err) console.log(err); // You know the drill, if it errors, tell the user
     data.replace('"', '“'); // Used to prevent errors in the JSON in relation to quotation characters
     data2 = csvParse(data); // Parse to an array
-    data2 = data2.map(array => array.filter((_, index) => (index + 1) % 3 === 0)); // Removes dates, and message ids, remove if you wish to keep
+    //data2 = data2.map(array => array.filter((_, index) => (index + 1) % 3 === 0)); // Removes dates, and message ids
 
     fs.writeFile('information.json', JSON.stringify(data2), function (err) { // Write the csv file contents to the JSON file
        if (err) console.log(err); // Ditto...
